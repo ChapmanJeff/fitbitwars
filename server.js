f9517b3a6d77096ae5a391d0f7022cae8f5da94f
Jeff Chapman <jeffcchapman@gmail.com>
2017-03-25 11:41:08 -0600

    Big Bang

---
+++ server.js
@@ -0,0 +1,35 @@
+var express = require('express');
+var bodyParser = require('body-parser')
+var session = require('express-session');
+var FitbitStrategy = require('passport-fitbit');
+var port = process.argv[2] || 8000;
+
+var app = express();
+
+app.use(express.statuc('public'));
+
+app.use(bodyParser.json());
+
+app.use(session({
+  secret: config.session.secret,
+  resave: true,
+  saveUninitialized: true
+}))
+
+passport.use(new FitbitStrategy({
+    consumerKey: FITBIT_CONSUMER_KEY,
+    consumerSecret: FITBIT_CONSUMER_SECRET,
+    callbackURL: "http://127.0.0.1:3000/auth/fitbit/callback"
+  },
+  function(token, tokenSecret, profile, cb) {
+    User.findOrCreate({ fitbitId: profile.id }, function (err, user) {
+      return cb(err, user);
+    });
+  }
+));
+
+
+
+
+
+app.listen(port, () => `listening on port ${port}`);
