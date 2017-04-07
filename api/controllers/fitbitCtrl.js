ff79fe139e59b4cd836c1f84b0c266d0b0951899
Jeff Chapman <jeffcchapman@gmail.com>
2017-04-07 14:06:17 -0600

    added activity to sql operation

--- api/controllers/fitbitCtrl.js
+++ api/controllers/fitbitCtrl.js
@@ -8,48 +8,73 @@ module.exports = {
     fitbitService.getDailyActivity(req.user.user_id, req.user.accesstoken)
       .then(function(response) {
         console.log(00, response);
+        var totalDistanceCalc = function(response){
+          for (var i = 0; i < response.summary.distances.length; i++) {
+            if (response.summary.distances[i].activity === "total") {
+              return response.summary.distances[i].distance;
+            }
+          }
+        };
+        var totalDistance = totalDistanceCalc();
+        console.log('totalDistance ', totalDistance);
+
+        db.activitySummary.insert({
+          user_id: req.user.user_id,
+          goal_activeMinutes: response.goal.activeMinutes,
+          goal_caloriesOut: response.goal.caloriesOut,
+          goal_distance: response.goal.distance,
+          goal_floors: response.goal.floors,
+          goal_steps: response.goal.steps,
+          summary_activeMinutes: response.summary.veryActiveMinutes,
+          summary_caloriesOut: response.summary.caloriesOut,
+          summary_distance: totalDistance,
+          summary_floors: response.summary.floors,
+          summary_steps: response.summary.steps,
+          summary_sedentaryMinutes: response.summary.sedentaryMinutes,
+          date: date
+        })
         res.send(response);
       })
 
   },
 
-  updateDailyActivity: function(nofitArr) {
-    for (var i = 0; i < nofitArr.length; i++) {
-      var user = db.profile.findOne({user_id: ownerId}, function(err, user){
-        return user;
-        console.log(0000,'this is the user ', user);
-      var date = nofitArr[i].date;
-      var ownerId = nofitArr[i].ownerId;
-      var accesstoken = user.accesstoken
-      var collectionType = nofitArr[i].collectionType;
-
-      fitbitService.updateDailyActivity(date, ownerId, accesstoken, collectionType)
-        .then(function(response) {
-          var activitySummaryId = db.run("select id from activitySummary where date Like %date% AND user_id Like %userID%", function(err, res) {
-            if (err || res.length > 1) {
-              console.log('Error: ',err, 'Result: ',res)
-            } else {
-              console.log('204 Result: ', res);
-              return res[0].id;
-            }
-          })
-
-          if (activitySummaryId) {
-            db.activitySummary.save({
-              id: activitySummaryId,
-              . . .
-            })
-          } else {
-            db.activitySummary.save({
-              user_id: ,
-              . . .
-            })
-          }
-        })
-    }
-  }
-
-}
+//   updateDailyActivity: function(nofitArr) {
+//     for (var i = 0; i < nofitArr.length; i++) {
+//       var user = db.profile.findOne({user_id: ownerId}, function(err, user){
+//         return user;
+//         console.log(0000,'this is the user ', user);
+//       var date = nofitArr[i].date;
+//       var ownerId = nofitArr[i].ownerId;
+//       var accesstoken = user.accesstoken
+//       var collectionType = nofitArr[i].collectionType;
+//
+//       fitbitService.updateDailyActivity(date, ownerId, accesstoken, collectionType)
+//         .then(function(response) {
+//           var activitySummaryId = db.run("select id from activitySummary where date Like %date% AND user_id Like %userID%", function(err, res) {
+//             if (err || res.length > 1) {
+//               console.log('Error: ',err, 'Result: ',res)
+//             } else {
+//               console.log('204 Result: ', res);
+//               return res[0].id;
+//             }
+//           })
+//
+//           if (activitySummaryId) {
+//             db.activitySummary.save({
+//               id: activitySummaryId,
+//               . . .
+//             })
+//           } else {
+//             db.activitySummary.save({
+//               user_id: ,
+//               . . .
+//             })
+//           }
+//         })
+//     }
+//   }
+//
+// }
 
 
 // const request = require('request');
@@ -81,4 +106,4 @@ module.exports = {
 //
 //   }
 //
-// }
+}
